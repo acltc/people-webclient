d108803eade891a415c9cf460a71a1d72d23d33c
Jay Wengrow <jaywngrw@gmail.com>
2014-07-23 16:50:40 -0500

    implemented edit and update

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -23,4 +23,20 @@ class PeopleController < ApplicationController
 
     redirect_to person_path(@person["id"])
   end
+
+  def edit
+    @person = Unirest.get("http://localhost:3000/people/#{params[:id]}.json", headers:{ "Accept" => "application/json" }).body
+  end
+
+  def update
+    @person = Unirest.patch("http://localhost:3000/people/#{params[:id]}.json", 
+              headers:{ "Accept" => "application/json" }, 
+              parameters:{ :person => 
+                {
+                  :first_name => params[:first_name], :middle_name => params[:middle_name], :last_name => params[:last_name], :suffix => params[:suffix], :email => params[:email], :phone => params[:phone]
+                }
+              }).body
+
+    redirect_to person_path(@person["id"])
+  end
 end
